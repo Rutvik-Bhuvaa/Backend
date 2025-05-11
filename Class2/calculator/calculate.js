5106d0c831d1de83c03456fe899cee78a1871fdb
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2025-05-11 09:43:59 +0530

    optimize way to use require and module.exports

---
+++ Class2/calculator/calculate.js
@@ -0,0 +1,6 @@
+const add = require("./addition");
+const sub = require("./subtraction");
+const mul = require("./multiply");
+const div = require("./division");
+
+module.exports = { add, sub, mul, div };
---
+++ Class2/first.js
@@ -0,0 +1,6 @@
+const { add, div, mul, sub } = require("./calculator/calculate");
+
+add(3, 4);
+sub(6, 3);
+mul(6, 3);
+div(6, 3);
