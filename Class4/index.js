30dca7b7bb8d61df21ce21f2f4b448fa0f6387b5
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2025-05-13 12:30:45 +0530

    Add the code to handle routing

--- Class4/index.js
+++ Class4/index.js
@@ -1,7 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello coder army");
+  if (req.url === "/") {
+    res.end("Hello coder army");
+  } else if (req.url === "/contact") {
+    res.end("This is our contact page");
+  } else if (req.url === "/about") {
+    res.end("This is our about page");
+  } else {
+    res.end("Page is not found");
+  }
 });
 
 server.listen(4000, () => {
