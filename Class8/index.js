71dfa0bdf314c0e5038a150bac690f856e6d77a8
RutvikBhuvaa <rutvikbhuvaa9@gmail.com>
2025-07-15 10:03:15 +0530

    You can't send response multiple times untill request comes

--- Class8/index.js
+++ Class8/index.js
@@ -4,13 +4,13 @@ const app = express();
 
 app.use(express.json());
 
-const BookStore = [
-  {
-    id: 1,
-    name: "Nexus",
-    author: "rohit",
-  },
-];
+app.use("/user", (req, res) => {
+  // here this is not persistant connection so it gives the error because it is a TCP connection
+  console.log("Hi there");
+  res.send("How are you?");
+  console.log("Kaise ho");
+  res.send("hello world");
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
